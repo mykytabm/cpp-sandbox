5edd6b1e24d1739af4c9144241ceae3116386d6a
nikitabm <nikitabalabuiev@gmail.com>
2021-02-25 00:11:58 +0100

    add random_device random

--- Test/Galaxy.cpp
+++ Test/Galaxy.cpp
@@ -1,6 +1,8 @@
 #include "olcPixelGameEngine.h"
+#include <random>
 
-// not finished
+// procedurally generated Galaxy
+
 class Galaxy : public olc::PixelGameEngine
 {
 public:
@@ -9,12 +11,9 @@ public:
 		sAppName = "Galaxy";
 	}
 private:
-	olc::vi2d worldSize = { 5,6 };
-	olc::vi2d vTileSize = { 128,64 };
-	olc::vi2d vOrigin = { 2,0 };
-	olc::Sprite* sprIsom = nullptr;
-	int* world = nullptr;
+
 public:
+
 	bool OnUserCreate() override
 	{
 		return true;
@@ -22,7 +21,30 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		Clear(olc::WHITE);
+		if (GetKey(olc::SPACE).bReleased) {
+
+			Clear(olc::BLACK);
+			std::random_device rd;
+			std::mt19937 mt(rd());
+			std::uniform_int_distribution<int> dist(0, 256);
+
+			auto genStartTime = std::chrono::system_clock::now();
+
+			for (int x = 0; x < ScreenWidth(); x++) {
+				for (int y = 0; y < ScreenHeight(); y++)
+				{
+					bool bIsStar = false;
+					int nSeed = y << 16 | x;
+
+					bIsStar = dist(mt) < 32;
+					Draw(x, y, bIsStar ? olc::WHITE : olc::BLACK);
+				}
+			}
+			auto genEndTime = std::chrono::system_clock::now();
+			std::chrono::duration<float> elapsedTime = genEndTime - genStartTime;
+			DrawString(3, 3, "Time: " + std::to_string(elapsedTime.count()), olc::RED, 2);
+		}
+
 
 		return true;
 	}
--- Test/Main.cpp
+++ Test/Main.cpp
@@ -2,8 +2,8 @@
 int main()
 {
 	Galaxy galaxy;
-	if (galaxy.Construct(640, 480, 1, 1));
-	galaxy.Start();
+	if (galaxy.Construct(640, 480, 1, 1))
+		galaxy.Start();
 
 	return 0;
 }
